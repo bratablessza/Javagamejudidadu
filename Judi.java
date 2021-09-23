5a07d176c3aad161424ef5a55c0b57bdfff763e3
Brata Blessza Cham <78901550+bratablessza@users.noreply.github.com>
2021-09-23 11:30:48 +0700

    Add files via upload

---
+++ Judi.java
@@ -0,0 +1,91 @@
+//import java.util.Scanner;
+//
+//public class Judi {
+//	public static void main(String[] args) {
+//		
+//		Scanner scn = new Scanner(System.in);
+//		
+//		System.out.println("\n");
+//		System.out.println("==============================================");
+//		System.out.println("=================== JUDIKA ===================");
+//		System.out.println("=============== Judi Dadu Kita ===============");
+//		System.out.println("============================================== \n");
+//		
+////		System.out.println("\n");
+////		System.out.print("Masukkan Nama Anda : ");
+////		String Nama = scn.nextLine();
+////		System.out.print("Masukkan Nominal Deposit : ");
+////		int Nominal = scn.nextInt();
+//		double Dadu = (Math.random()*5)+1+1;;
+//		
+//		System.out.println((int)Dadu);
+////		
+////	boolean Ulang = true;
+////	
+////	while(Ulang) {	
+////		
+////		
+////	if(Nominal < 50) {
+////		System.out.println(Nama + " Saldo gacukup");
+////		break;
+////	}
+////	
+////	System.out.println("Tebak angka berapa yg keluar");
+////	int Taruh =scn.nextInt();
+////	
+////	
+////	if(Taruh == ((int)Dadu)) {
+////		Nominal = (Nominal *2) - 50;
+////		System.out.println("Tebakan Jitu");
+////		System.out.println("Dadu yg keluar : " +(int)Dadu);
+////		System.out.println("\n");
+////		break;
+////	} else {
+////		System.out.println("Tebakan gagal");
+////		System.out.println("Dadu yg keluar : " +(int)Dadu);
+////		System.out.println("\n");
+////		Nominal = Nominal - 50;
+////		break;
+////		
+////	}	
+////
+////	
+////}
+////		
+////	
+////	
+////	System.out.println("\nSisa saldo " +Nama+ " adalah : " + Nominal);
+////	
+////
+////	
+////			
+////	
+////
+//////	while(Ulang) {
+////////		Nominal =scn.hasNextInt();
+//////		Nominal = Nominal - 50;
+//////		
+//////		if(Nominal <50) {
+//////			System.out.println("Saldo gacukup");
+//////			break;
+//////		}
+//////		
+//////		if(Taruh == Dadu) {
+//////			Nominal = Nominal *2;
+//////			System.out.println("Tebakan Jitu");
+//////		}
+//////		
+//////		
+//////		
+//////	}
+////	
+////	
+////	
+////	
+////	System.out.println("\n\n==============================================");
+////	System.out.println("====TERIMAKASIH TELAH BERMAIN BERSAMA KAMI====");
+////	System.out.println("==============================================");
+////
+//	}
+//}
+//
